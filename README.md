213ad15b17d96059555992355057d4a9b08fdb2b
Bhaskar Kumar <thisisofficialbk@gmail.com>
2022-01-30 21:23:34 +0530

    How to use body parser to tap into body and get variable

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+# Calculator Project
+
 Step 1. Setup the server locally  
 Step 2. sending the html file through server using res.sendFile method   
+Step 3. body-parser package works with express  
+Step 4. So for using it for FORM we use app.use(bodyParser.urlencoded({extended :true}))  
+Step 5. Keep eye on how body-parser working Hint we are posting on post request
--- calculator.js
+++ calculator.js
@@ -1,9 +1,20 @@
 const express= require("express")
 const app =express();
-
+const bodyParser=require("body-parser")
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req, res){
 res.sendFile(__dirname + "/index.html");
 });
+// now create post method for posting in home route
+app.post("/",function(req, res){
+  // console.log(req.body.num1);
+  var num1= Number(req.body.num1);
+  var num2 =Number(req.body.num2);
+  var result= num1 +num2;
+
+  res.send("The Result of the calculation is " +result);
+});
+
 app.listen(3000,function(req,res){
   console.log("Server Started on port 3000");
 });
