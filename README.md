d327311823cc2c14e40cfc6f227f39880b2d58e2
Bhaskar Kumar <thisisofficialbk@gmail.com>
2022-01-30 20:43:41 +0530

    Sent html file through server

---
+++ README.md
@@ -0,0 +1,2 @@
+Step 1. Setup the server locally  
+Step 2. sending the html file through server using res.sendFile method   
--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ const express= require("express")
 const app =express();
 
 app.get("/",function(req, res){
-  res.send("Hello World")
+res.sendFile(__dirname + "/index.html");
 });
 app.listen(3000,function(req,res){
   console.log("Server Started on port 3000");
